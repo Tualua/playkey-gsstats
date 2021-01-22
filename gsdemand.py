e4c54fd9b078bb88a61e3134180d70a5bc0a2e07
dsp <dsp@pk-host3.pk.minecolo.io>
2021-01-22 13:24:33 +0700

    Initial

---
+++ gsdemand.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+import geoip2.database
+import xml.etree.ElementTree as ET
+import subprocess
+
+
+# Read VM names from GameServer config
+def get_servers(path="/usr/local/etc/gameserver/conf.xml"):
+    tree = ET.parse(path)
+    root = tree.getroot()
+    vms = []
+    for server in root.iter('Server'):
+        vms.append(server.get('name'))
+    return vms
+
+
+# Wrapper for shell command excecution
+def exec_shell_command(command):
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    stdout, stderr = proc.communicate()
+    return stdout.split('\n')
+
+
+# Read GameServer log for <vm> for <start>-<end> timeframe.
+def get_log(domainName, start, end, reverse=False, debug=False, logdir=''):
+    jctl_options = ["-o short-iso"]
+    jctl_options.append("--no-pager -tgameserver/{}".format(domainName))
+
+    if start:
+        jctl_options.append("--since=\"{}\"".format(start))
+    if end:
+        jctl_options.append("--until=\"{}\"".format(end))
+    if reverse:
+        jctl_options.append("-r")
+    if logdir:
+        jctl_options.append("--directory {}".format(logdir))
+
+    jctl = "journalctl {}".format(" ".join(jctl_options))
+    if debug:
+        print('Get journal: {}'.format(jctl))
+    data = exec_shell_command(jctl)
+
+    return data
+
+
+# Get IP address location info using MaxMind databases
+def get_location(ipaddress):
+    reader = geoip2.database.Reader('/usr/share/GeoIP/GeoLite2-City.mmdb')
+    readerasn = geoip2.database.Reader('/usr/share/GeoIP/GeoLite2-ASN.mmdb')
+    result = {}
+    try:
+        location = reader.city(ipaddress)
+        result["Continent"] = location.continent.name.encode('ascii', 'replace')
+        result["Country"] = location.country.name.encode('ascii', 'replace')
+        result["Region"] = location.subdivisions.most_specific.name.encode('ascii', 'replace')
+        result["City"] = location.city.name.encode('ascii', 'replace')
+        result["Latitude"] = float(location.location.latitude)
+        result["Longitude"] = float(location.location.longitude)
+        asn = readerasn.asn(ipaddress)
+        result["ASN"] = "AS{}".format(asn.autonomous_system_number)
+        result["ASN Provider"] = asn.autonomous_system_organization.encode('ascii', 'replace')
+    except geoip2.errors.AddressNotFoundError:
+        pass
+    except AttributeError:
+        pass
+    reader.close()
+    readerasn.close()
+    return result
